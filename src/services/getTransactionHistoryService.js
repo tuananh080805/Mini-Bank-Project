7fb203c2d1919fbbe95391ef0096f5656a643bfc
tuananhlaptrinh <bui058277@gmail.com>
2026-03-05 11:19:33 +0700

    Update History Transaction

--- src/services/getTransactionHistoryService.js
+++ src/services/getTransactionHistoryService.js
@@ -23,12 +23,22 @@ const getHistory = async (userId) => {
                 include: {
                     bank_accounts_transactions_from_account_idTobank_accounts: {
                         select: {
-                            account_number: true
+                            account_number: true,
+                            users: {
+                                select: {
+                                    email: true
+                                }
+                            }
                         }
                     },
                     bank_accounts_transactions_to_account_idTobank_accounts: {
                         select: {
-                            account_number:true
+                            account_number: true,
+                            users: {
+                                select: {
+                                    email: true
+                                }
+                            }
                         }
                     }
                 },
@@ -39,7 +49,9 @@ const getHistory = async (userId) => {
             const history = result.map((data) => ({
                 reference: data.reference_number?.toString(),
                 fromAccount: data.bank_accounts_transactions_from_account_idTobank_accounts ?.account_number?.toString() || null,
+                fromUser: data.bank_accounts_transactions_from_account_idTobank_accounts?.users?.email || null,
                 toAccount: data.bank_accounts_transactions_to_account_idTobank_accounts?.account_number?.toString() || null,
+                toUser: data.bank_accounts_transactions_to_account_idTobank_accounts?.users?.email || null,
                 amount: data.amount?.toString(),
                 type: data.type,
                 status: data.status,
